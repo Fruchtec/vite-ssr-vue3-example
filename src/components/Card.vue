f3f4a2451aac08a53ea6961e70f63ea0c51dca7d
Pascal Klau <email@artofmyself.com>
2022-02-08 12:33:05 +0100

    clean-up

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -15,22 +15,20 @@ defineProps<Props>()
 </script>
 
 <template>
-  <RouterLink :to="{ name: 'Product', params: { id: product.id } }" class="card">
-    <div class="card-img">
-      <img
-        loading="lazy"
-        :src="product.image"
-        alt="image of card"
-      >
+  <div class="card-img">
+    <img
+      loading="lazy"
+      :src="product.image"
+      alt="image of card"
+    >
+  </div>
+  <div class="card-body">
+    <h4 class="card-title text-center">
+      {{ product.title }}
+    </h4>
+    <small class="card-detail">{{ product.description }}</small>
+    <div class="card-price">
+      {{ product.price }}$
     </div>
-    <div class="card-body">
-      <h4 class="card-title text-center">
-        {{ product.title }}
-      </h4>
-      <small class="card-detail">{{ product.description }}</small>
-      <div class="card-price">
-        {{ product.price }}$
-      </div>
-    </div>
-  </RouterLink>
+  </div>
 </template>
